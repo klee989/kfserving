a8c39e0e232f8cc0ea912c6316d7d469373efe6f
klee989 <55095912+klee989@users.noreply.github.com>
2019-11-18 11:13:00 -0500

    Update inferencerouter_types.go

--- pkg/apis/serving/v1alpha2/inferencerouter_types.go
+++ pkg/apis/serving/v1alpha2/inferencerouter_types.go
@@ -1,3 +1,25 @@
+/*
+Copyright 2019 kubeflow.org.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha2
+
+import (
+	"github.com/kubeflow/kfserving/pkg/constants"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	duckv1beta1 "knative.dev/pkg/apis/duck/v1beta1"
+)
+
 // ABTestSpec defines parameters required for an A/B test.
 type ABTestSpec {
 	// Name of numeric metric we are A/B testing for improvement.
